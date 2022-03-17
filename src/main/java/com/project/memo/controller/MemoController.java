870e399a23ab990108ce2b142254c993aeb44326
pypygeek <pypygeek@gmail.com>
2022-03-17 21:11:48 +0900

    First Commit

---
+++ src/main/java/com/project/memo/controller/MemoController.java
@@ -0,0 +1,44 @@
+package com.project.memo.controller;
+
+import com.project.memo.domain.Memo;
+import com.project.memo.domain.MemoRepository;
+import com.project.memo.domain.MemoRequestDto;
+import com.project.memo.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MemoController {
+
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+    @GetMapping("/api/memos")
+    public List<Memo> getMemos() {
+        LocalDateTime start = LocalDateTime.now().minusDays(1);
+        LocalDateTime end = LocalDateTime.now();
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start, end);
+    }
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        memoService.update(id, requestDto);
+        return id;
+    }
+
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        memoRepository.deleteById(id);
+        return id;
+    }
+}
---
+++ src/main/java/com/project/memo/service/MemoService.java
@@ -0,0 +1,25 @@
+package com.project.memo.service;
+
+import com.project.memo.domain.Memo;
+import com.project.memo.domain.MemoRepository;
+import com.project.memo.domain.MemoRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional
+    public Long update(Long id, MemoRequestDto requestDto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        memo.update(requestDto);
+        return memo.getId();
+    }
+}
